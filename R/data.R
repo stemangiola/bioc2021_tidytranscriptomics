3c0f9db44a2f66888a6ce19bd4520b69818f85a6
stemangiola <mangiolastefano@gmail.com>
2021-02-28 20:09:28 +1100

    first commit

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' BRCA
+#'
+#' Subset of the TCGA BRCA data set
+#'
+#' @format  A tidy data frame.
+#' @usage data(BRCA)
+"BRCA"
+
+#' pasilla
+#'
+#' pasilla gene count data in SummarizedExperiment format
+#'
+#' @format  A SummarizedExperiment object with the 7 pasilla samples. Generated from the code in data-raw/pasilla.R.
+#' @source \url{https://bioconductor.org/packages/release/data/experiment/html/pasilla.html}
+#' @usage data(pasilla)
+"pasilla"
+
+#' pbmc
+#'
+#' A SingleCellExperiment dadaset of single cell RNA sequencing data
+#'
+#' @format  A Seurat object.
+#' @usage data(pbmc)
+"pbmc"
+
+#' de_gate_gates
+#'
+#' Stefano's gate list
+#'
+#' @format  A list of gates to use. Gates consist of x and y coordinates.
+#' @usage data(de_gate_gates)
+"de_gate_gates"
