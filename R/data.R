71a360326b632498c0c2801ac4f562b0af64d97a
stemangiola <mangiolastefano@gmail.com>
2021-06-09 19:56:48 +1000

    add excercises

--- R/data.R
+++ R/data.R
@@ -8,8 +8,16 @@
 
 #' pbmc
 #'
-#' A SingleCellExperiment dadaset of single cell RNA sequencing data
+#' A SingleCellExperiment dataset of single cell RNA sequencing data
 #'
 #' @format  A Seurat object.
 #' @usage data(pbmc)
 "pbmc"
+
+#' mito_info_all_datasets
+#'
+#' A SingleCellExperiment dataset of single cell RNA sequencing data. Used for one of the exercises
+#'
+#' @format  A Seurat object.
+#' @usage data(mito_info_all_datasets)
+"mito_info_all_datasets"
